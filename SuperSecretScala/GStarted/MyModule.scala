a85bbef841392172799cb9c13da3ff9effc4a26b
Paul Arizpe <kiramishima@outlook.com>
2016-09-14 01:48:40 -0500

    factorial

--- SuperSecretScala/GStarted/MyModule.scala
+++ SuperSecretScala/GStarted/MyModule.scala
@@ -6,6 +6,21 @@ object MyModule {
         if (n < 0) -n
         else n
 
+    def factorial(n: Int): Int = {
+        /** An inner function, or local definition.
+        It’s common in Scala to write
+        functions that are local to the body
+        of another function. In functional
+        programming, we shouldn’t consider
+        this a bigger deal than local integers
+        or strings.*/
+        def go(n: Int, acc: Int): Int =
+            if (n <= 0) acc
+            else go(n-1, n*acc)
+
+        go(n, 1)
+    }
+
     private def formatAbs(x: Int) = {
         val msg = "The absolute value of %d is %d"
         msg.format(x, abs(x))
