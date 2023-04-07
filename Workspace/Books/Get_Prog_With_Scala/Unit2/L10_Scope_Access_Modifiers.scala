337bbe9c9068ef3e68d545d96bc57bc12cd25a6b
Kira Mishima <kiramishima@outlook.com>
2023-04-07 15:14:49 -0600

    Fix archivos en directorio correcto

---
+++ Workspace/Books/Get_Prog_With_Scala/Unit2/L10_Scope_Access_Modifiers.scala
@@ -0,0 +1,37 @@
+
+class Parti {
+    // var attendees = 0 // public
+    private var attendees = 0 //private
+
+    def register(guests: Int) =
+        attendees += guests
+}
+
+class Test {
+    val configA = "I am public"
+    private val configB = "I am private"
+}
+
+class Event {
+    // protected
+    protected def estimateCosts(attendees: Int): Double =
+        if (attendees < 10) 50.00 else attendees * 12.34
+}
+
+class Party extends  Event {
+    private var attendees = 0
+    var cost = estimateCosts(attendees)
+
+    def register(guests: Int) =
+        attendees += guests
+}
+
+object Main extends App {
+    val party1 = new Party
+    party1.register(5)
+    println(party1.cost)
+
+    
+    party1.register(10)
+    println(party1.cost)
+}
